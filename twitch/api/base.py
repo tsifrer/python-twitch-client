d4cf825e0578312a3295348fa29fc185eb7048cf
Volcyy <Johannes.christ@gmail.com>
2017-08-22 19:06:18 +0200

    Inlined the backoff function.

--- twitch/api/base.py
+++ twitch/api/base.py
@@ -6,19 +6,6 @@ from requests.compat import urljoin
 from twitch.constants import BASE_URL
 
 
-def _backoff(url, params, headers, original_response, initial_backoff=0.5, max_retries=3):
-    backoff = initial_backoff
-
-    for _ in range(max_retries):
-        time.sleep(backoff)
-        response = requests.get(url, params=params, headers=headers)
-        if response.status_code < 500:
-            return response
-        backoff *= 2
-
-    return original_response
-
-
 class TwitchAPI(object):
 
     def __init__(self, client_id, oauth_token=None, *args, **kwargs):
@@ -44,7 +31,16 @@ class TwitchAPI(object):
 
         response = requests.get(url, params=params, headers=headers)
         if response.status_code >= 500:
-            response = _backoff(url, params, headers, response)
+
+            backoff = 0.5
+            for _ in range(3):
+                time.sleep(backoff)
+                backoff_response = requests.get(url, params=params, headers=headers)
+                if backoff_response.status_code < 500:
+                    response = backoff_response
+                    break
+                backoff *= 2
+
         response.raise_for_status()
         return response.json()
 
