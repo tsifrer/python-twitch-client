a73e6572c1e435835fdd15b8f31eb596225d16b0
Volcyy <Johannes.christ@gmail.com>
2017-08-22 15:02:33 +0200

    Adds a simple handler for >= 500 server errors.
    As described in #16, this implements a simple function `_backoff`,
    which attempts to get a server response with a status code below 500.
    Fixes #16.

--- twitch/api/base.py
+++ twitch/api/base.py
@@ -1,7 +1,22 @@
+import time
+
 import requests
 from requests.compat import urljoin
 
 from twitch.constants import BASE_URL
+
+
+def _backoff(url, params, headers, original_response, initial_backoff=0.5, max_retries=3):
+    backoff = initial_backoff
+
+    for _ in range(max_retries):
+        time.sleep(backoff)
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code < 500:
+            return response
+        backoff *= 2
+
+    return original_response
 
 
 class TwitchAPI(object):
@@ -28,6 +43,8 @@ class TwitchAPI(object):
         headers = self._get_request_headers()
 
         response = requests.get(url, params=params, headers=headers)
+        if response.status_code >= 500:
+            response = _backoff(url, params, headers, response)
         response.raise_for_status()
         return response.json()
 
