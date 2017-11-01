d426b2249b554144d3e90b10f0edfe5b88729b39
Bruno <garcia.bruno0@gmail.com>
2017-11-01 07:14:55 -0300

    Added test_get_top

--- tests/api/test_clips.py
+++ tests/api/test_clips.py
@@ -13,6 +13,8 @@ example_clip = {
     "url": "https://clips.twitch.tv/OpenUglySnoodVoteNay?tt_medium=clips_api&tt_content=url"
 }
 
+example_clips = {'clips': [example_clip]}
+
 @responses.activate
 def test_get_slug():
     slug = 'OpenUglySnoodVoteNay'
@@ -30,3 +32,23 @@ def test_get_slug():
 
     assert isinstance(clip, Clip)
     assert clip.broadcast_id == example_clip['broadcast_id']
+
+
+@responses.activate
+def test_get_top():
+    params = {'limit': 1, 'period': 'month'}
+
+    responses.add(
+        responses.GET,
+        '%sclips/top' % BASE_URL,
+        body=json.dumps(example_clips),
+        status=200,
+        content_type='application/json'
+    )
+
+    client = TwitchClient('client id')
+    clips = client.clips.get_top(**params)
+
+    assert len(clips) == len(example_clips)
+    assert isinstance(clips[0], Clip)
+    assert clips[0].broadcast_id == example_clips['clips'][0]['broadcast_id']
