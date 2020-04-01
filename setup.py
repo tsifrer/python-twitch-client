f8aabf8611867dc9d4a75f561b22c77945d731d4
Jonne Kaunisto <jonneka@gmail.com>
2020-04-01 12:36:27 -0700

    added setup.py to contain dev requirements

--- setup.py
+++ setup.py
@@ -3,14 +3,76 @@ import sys
 from codecs import open
 
 import setuptools
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    """Handle test execution from setup."""
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass into pytest")]
+
+    def initialize_options(self):
+        print("here")
+        """Initialize the PyTest options."""
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def finalize_options(self):
+        """Finalize the PyTest options."""
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        """Run the PyTest testing suite."""
+        try:
+            import pytest
+        except ImportError:
+            raise ImportError('Running tests requires additional dependencies.'
+                              '\nPlease run (pip install python-twitch-client[test])')
+
+        errno = pytest.main(self.pytest_args.split(" "))
+        sys.exit(errno)
+
+
+cmdclass = {'test': PyTest}  # Define custom commands.
+
+
+if "build_docs" in sys.argv:
+    try:
+        from sphinx.setup_command import BuildDoc
+    except ImportError:
+        raise ImportError(
+            'Running the documenation builds has additional'
+            ' dependencies. Please run (pip install python-twitch-client[doc])'
+        )
+    cmdclass["build_docs"] = BuildDoc
 
 requires = [
     'requests>=2.18.4',
     'six>=1.11.0'
 ]
 
+test_reqs = [
+    'codecov==2.0.15',
+    'configparser==3.5.0',
+    'flake8-import-order==0.17.1',
+    'flake8==3.5.0',
+    'pycodestyle==2.3.1',
+    'pytest-cov==2.5.1',
+    'pytest==3.5.1',
+    'responses==0.9.0'
+]
+
+doc_reqs = [
+    'Sphinx==1.7.4',
+    'sphinx-autobuild==0.7.1',
+    'sphinx_rtd_theme==0.3.1'
+]
+
 extras_require = {
     ":python_version<'3.2'": ['configparser'],
+    "doc": doc_reqs,
+    "test": test_reqs
 }
 
 if int(setuptools.__version__.split('.')[0]) < 18:
@@ -33,7 +95,9 @@ setuptools.setup(
     author_email='tomazz.sifrer@gmail.com',
     url='https://github.com/tsifrer/python-twitch-client',
     packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    cmdclass=cmdclass,
     install_requires=requires,
+    tests_require=test_reqs,
     extras_require=extras_require,
     license='MIT',
     zip_safe=False,
