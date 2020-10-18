ac9d7342c2f2e8b22a5acbf790e5781ce27666b7
Ville Skyttä <ville.skytta@iki.fi>
2020-10-18 10:44:43 +0300

    Do not install samples

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setuptools.setup(
     author="Tomaz Sifrer",
     author_email="tomazz.sifrer@gmail.com",
     url="https://github.com/tsifrer/python-twitch-client",
-    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    packages=setuptools.find_packages(exclude=["tests", "tests.*", "samples", "samples.*"]),
     cmdclass=cmdclass,
     install_requires=requires,
     tests_require=test_requirements,
